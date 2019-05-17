910efcb4da293a0a22243de7b5e290c882d425c2
Damien Laureaux <d.laureaux@timoa.com>
2019-05-17 22:54:41 +0100

    Update the configController to provide a custom playlist or default

--- src/controllers/configController.js
+++ src/controllers/configController.js
@@ -1,13 +1,62 @@
 const boom = require('boom');
+const fs = require('fs');
+const path = require('path');
+
+const logger = require('../lib/logger');
+const inventory = require('../static/inventory.json');
+
+const getConfigPath = (request) => {
+  
+  // Default
+  let pathname = path.join(__dirname, '../static/playlists');
+  let filename = 'default.json';
+
+  // Detect the screen by IP. Support detection with or without proxy
+  const ip = request.headers['x-forwarded-for'] || request.ip.substring(7);
+
+  // Specific screen
+  inventory.screens.forEach((item) => {
+    if (item.ip === ip) {
+      filename = `${item.name}.json`;
+    }
+  });
+
+  pathname = path.join(pathname, filename);
+
+  logger.info(`[${ip}] ${request.method} ${request.url}  => ${filename}`);
+
+  return pathname;
+};
 
 // Get secrets by ID
-exports.getConfig = async req => {
+exports.getConfig = async (request, reply) => {
   try {
-    let json = {};
-    if (req.body) {
-      json = JSON.stringify(req.body);
-    }
-    return json;
+    const pathname = getConfigPath(request);
+    logger.info(pathname);
+
+    fs.exists(pathname, (exist) => {
+      if (!exist) {
+        // if the file is not found, return 404
+        reply
+          .code(404)
+          .send({
+            error: `File ${pathname} not found!`
+          });
+      }
+      fs.readFile(pathname, (err, data) => {
+        if (err) {
+          reply
+            .code(500)
+            .send({
+              error: err
+            });
+        } else {
+          reply
+            .header('Content-type', 'application/json')
+            .send(data);
+        }
+      });
+    });
   } catch (err) {
     throw boom.boomify(err);
   }
