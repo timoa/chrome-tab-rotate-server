605d93e8b099833555ba02d97f43dc3246b33ba4
Damien Laureaux <d.laureaux@timoa.com>
2019-05-20 21:03:57 +0100

    Add additional tests for the playlist library

--- test/lib.playlist.spec.js
+++ test/lib.playlist.spec.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
+
 const lib = require('../src/lib/playlist');
+const testData = require('./testData.json');
 
 // Lib > Playlist
 describe('Playlist library', () => {
@@ -9,6 +11,11 @@ describe('Playlist library', () => {
   });
   it('expect the "getPlaylistPath" function to throw without an argument', done => {
     expect(lib.getPlaylistPath).to.throw();
+    done();
+  });
+  it('expect the "getPlaylistPath" function to throw without an argument', done => {
+    const screen01Path = `${(__dirname).replace(/test/, '')}src/static/playlists/screen01.json`;
+    expect(lib.getPlaylistPath(testData.network[0].request)).to.be.equals(screen01Path);
     done();
   });
 });
