350037fa1d67398cbc3c0da67865021e73763e86
Damien Laureaux <d.laureaux@timoa.com>
2020-07-14 10:32:05 +0200

    Update the functional tests URL

--- test/functional/api.spec.js
+++ test/functional/api.spec.js
@@ -5,7 +5,7 @@ const config = require('../../src/config/config.json');
 // API
 describe('Root API endpoint response', () => {
   it('should return 404', done => {
-    request.get(`http://localhost:${config.app.port}/api`, (err, res) => {
+    request.get(`http://0.0.0.0:${config.app.port}/api`, (err, res) => {
       if (err) throw err;
       expect(res.statusCode).to.equal(404);
       done();
--- test/functional/playlist.spec.js
+++ test/functional/playlist.spec.js
@@ -5,7 +5,7 @@ const config = require('../../src/config/config.json');
 // Server
 describe('Config endpoint response', () => {
   it('should return 200', done => {
-    request.get(`http://localhost:${config.app.port}/`, (err, res) => {
+    request.get(`http://0.0.0.0:${config.app.port}/`, (err, res) => {
       if (err) throw err;
       expect(res.statusCode).to.equal(200);
       done();
