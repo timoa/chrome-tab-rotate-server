05c565233c106e5d98bd2b0d02469bad7342f7bb
Damien Laureaux <d.laureaux@timoa.com>
2023-02-10 09:02:38 +0100

    fix(deps): update Fastify to v4.x

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -1,4 +1,4 @@
-const boom = require('boom');
+const boom = require('@hapi/boom');
 const path = require('path');
 const fs = require('fs');
 
--- src/controllers/playlistController.js
+++ src/controllers/playlistController.js
@@ -1,4 +1,4 @@
-const boom = require('boom');
+const boom = require('@hapi/boom');
 const fs = require('fs');
 
 const playlist = require('../lib/playlist');
